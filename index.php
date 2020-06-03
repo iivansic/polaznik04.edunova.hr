4034687c6cd66a83f11912eb7dc8bcb385841352
iivansic <ivan.ivansic@sdfgroup.com>
2020-06-03 22:47:27 +0200

    git ignore i pocetak php

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foundation for Sites</title>
+    <link rel="stylesheet" href="assets/css/foundation.css">
+  </head>
+<body>
+
+<?php
+/*
+header("Location: https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+echo "<script>location='https://www.youtube.com/watch?v=dQw4w9WgXcQ'</script>";
+*/
+?>
+
+<script src="assets/js/vendor.js"></script>
+<script src="assets/js/foundation.js"></script>
+<script src="assets/js/app.js"></script>
+</body>
+</html>
