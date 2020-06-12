34705fb1372520558c89b2dd477bb760a3b51798
iivansic <ivan.ivansic@sdfgroup.com>
2020-06-12 22:22:45 +0200

    nastavak vjezbe na css i html

--- zadatak1.php
+++ zadatak1.php
@@ -2,4 +2,4 @@
 $b1=$_GET['b1'];
 $b2=$_GET['b2'];
 echo "<table style='width:100%'>","<tr>","<td>", $b1 + $b2,"</td>","<td>", $b1 - $b2,"</td>","<td>",
- $b1 * $b2,"</td>","<td>", $b1/$b2 ,"</td>","</tr>","</table>";
+$b1 * $b2,"</td>","<td>", $b1/$b2 ,"</td>","</tr>","</table>";
