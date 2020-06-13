050bca0804473c25e62d4cf478a3370706da3d14
iivansic <ivan.ivansic@sdfgroup.com>
2020-06-13 21:01:07 +0200

    pokušaj promjene alert msgboxa u nest bolje zasad ne ide

--- zadatak1.php
+++
@@ -1,5 +0,0 @@
-<?php
-$b1=$_GET['b1'];
-$b2=$_GET['b2'];
-echo "<table style='width:100%'>","<tr>","<td>", $b1 + $b2,"</td>","<td>", $b1 - $b2,"</td>","<td>",
-$b1 * $b2,"</td>","<td>", $b1/$b2 ,"</td>","</tr>","</table>";
